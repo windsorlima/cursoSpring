24aa8dcf89b24a78365023b723e5f88cc5262fde
Windsor Lima <windsorlimad@gmail.com>
2019-08-03 02:29:40 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/windsorlima/cursoSpring/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.windsorlima.cursoSpring.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.windsorlima.cursoSpring.domain.Cliente;
+import com.windsorlima.cursoSpring.repositories.ClienteRepository;
+import com.windsorlima.cursoSpring.services.exceptions.ObjectNotFoundExcception;
+
+@Service
+public class ClienteService {
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundExcception("Objeto não encontrado! Id: "+ id 
+					+ ", Tipo: "+ Cliente.class.getName());
+		}
+		return obj;
+	}
+}
