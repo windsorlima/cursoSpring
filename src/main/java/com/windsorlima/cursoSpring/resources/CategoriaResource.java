114fae7f5dceb628089a0c02a53a2f192945cffb
Windsor Lima <windsorlimad@gmail.com>
2019-08-08 22:22:11 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/windsorlima/cursoSpring/resources/CategoriaResource.java
+++ src/main/java/com/windsorlima/cursoSpring/resources/CategoriaResource.java
@@ -14,6 +14,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.windsorlima.cursoSpring.domain.Categoria;
 import com.windsorlima.cursoSpring.services.CategoriaService;
 
+import sun.security.provider.certpath.ResponderId;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
@@ -22,8 +24,8 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@RequestMapping(value="/{id}",method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok(obj);
 	}
 	
@@ -32,7 +34,13 @@ public class CategoriaResource {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.
 				fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).build();
-		
+		return ResponseEntity.created(uri).build();	
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 }
