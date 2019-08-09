0fd68c12af16360f8ed17d85c7b20d78602b9c61
Windsor Lima <windsorlimad@gmail.com>
2019-08-08 22:12:01 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/windsorlima/cursoSpring/resources/CategoriaResource.java
+++ src/main/java/com/windsorlima/cursoSpring/resources/CategoriaResource.java
@@ -1,14 +1,15 @@
 package com.windsorlima.cursoSpring.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.windsorlima.cursoSpring.domain.Categoria;
 import com.windsorlima.cursoSpring.services.CategoriaService;
@@ -24,5 +25,14 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.
+				fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+		
 	}
 }
--- src/main/java/com/windsorlima/cursoSpring/services/CategoriaService.java
+++ src/main/java/com/windsorlima/cursoSpring/services/CategoriaService.java
@@ -20,4 +20,9 @@ public class CategoriaService {
 		}
 		return obj;
 	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 }
